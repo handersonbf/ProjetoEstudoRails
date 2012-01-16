c55f541cf02fdc49da33583738ca4d330db5ba50
Handerson Frota <handersonbf@gmail.com>
2012-01-16 17:52:37 -0300

    configuracao do deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,22 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "ProjetoEstudoRails"
+set :scm, :git
+set :repository,  "git@github.com:handersonbf/ProjetoEstudoRails.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
 
-set :scm, :subversion
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, "your web-server here"                          # Your HTTP server, Apache/etc
 role :app, "your app-server here"                          # This may be the same as your `Web` server
 role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 role :db,  "your slave db-server here"
+
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
